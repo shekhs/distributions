2d91e113430d5d75213f5784b3a6e0102fc2538a
shekhs <31767433+shekhs@users.noreply.github.com>
2020-07-12 02:38:21 +0530

    Add files via upload
    a general python class distribution.
    Gaussian distribution with all its properties like mean and std dev. Same for Binomial distribution.
    Overwritten the + for adding two distribution properties statistically.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussbin',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['gaussbin'],
+      zip_safe=False)
